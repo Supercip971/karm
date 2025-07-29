da22424c1216a6dc657c568052dde3a3c135662f
sleepy-monax <nicolas.van.bossuyt@gmail.com>
2025-07-29 18:21:38 +0000

    Deploying to gh-pages from @ skift-org/karm@91e83027ca2760ff5fa824b5d304a983cad1aaa8 🚀

--- structKarm_1_1Ui_1_1Image.js
+++ structKarm_1_1Ui_1_1Image.js
@@ -2,5 +2,5 @@ var structKarm_1_1Ui_1_1Image =
 [
     [ "Image", "structKarm_1_1Ui_1_1Image.html#aebce8efb03f94c7f24d25d0c26903d37", null ],
     [ "paint", "structKarm_1_1Ui_1_1Image.html#a188e9e50ed8828e463bb02f5846b74c9", null ],
-    [ "size", "structKarm_1_1Ui_1_1Image.html#a7af247587ac53dc23ef96ccef7f93769", null ]
+    [ "size", "structKarm_1_1Ui_1_1Image.html#a26b50cdbe1c717a0808744dd3450a886", null ]
 ];
